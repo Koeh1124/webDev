1e857ab2dca5159087b61c0e72907c46df142234
Koeh1124 <keenan.koehler@evsck12.com>
2022-06-20 12:12:37 -0500

    started work on portfolio and added side menue

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+//sets the width so it opens the side bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+// "closes" the bar by setting the width to o
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
